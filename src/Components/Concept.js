9e3c23d94b0bb76adc6c50cda3dd07eae38b07d1
Junaid Shaikh <139779366+JunaidShaikh01@users.noreply.github.com>
2023-08-31 14:50:57 +0530

    Initial commit

---
+++ src/Components/Concept.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Concept (props){
+  return (
+    <li className="concept">
+    <img src={props.image} alt="TODO: TITLE" />
+    <h2>{props.tsitle}</h2>
+    <p>{props.description}</p>
+  </li>
+  )
+}
+export default Concept
